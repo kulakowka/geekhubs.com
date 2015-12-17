6f38c92d5dc1ced7d5afbc77831548ac02a5b52f
kulakowka <kulakowka@gmail.com>
2015-12-17 19:17:08 +0300

    reporter changed

--- seed/reporter.js
+++ seed/reporter.js
@@ -6,21 +6,21 @@ module.exports = function report (err, results) {
   if (err) console.log('Seeds result ERROR:', err)
 
   console.log('\nCreate Users...'.blue)
-  results.users.forEach(user => console.log('- %s'.green, user.username, user._id))
+  results.users.forEach(user => console.log('- %s'.green, user.username))
 
   console.log('\nCreate Hubs...'.blue)
-  results.hubs.forEach(hub => console.log('- %s'.green, hub.title, hub._id))
+  results.hubs.forEach(hub => console.log('- %s'.green, hub.title))
 
   console.log('\nCreate Articles...'.blue)
-  results.articles.forEach(article => console.log('- %s'.green, article.title, article._id))
+  results.articles.forEach(article => console.log('- %s'.green, article.title))
 
   console.log('\nCreate Comments...'.blue)
-  results.comments.forEach(comment => console.log('- %s'.green, comment.content, comment._id))
+  results.comments.forEach(comment => console.log('- %s'.green, comment.content))
 
   console.log('\nTotal:'.blue)
   console.log('Users:', colors.green(results.users.length))
   console.log('Hubs:', colors.green(results.hubs.length))
   console.log('Articles:', colors.green(results.articles.length))
   console.log('Comments:', colors.green(results.comments.length))
-  console.log('\nClose connection.\n'.red)
+  console.log('\nClose connection. Press Ctrl + C to finish.\n'.red)
 }
