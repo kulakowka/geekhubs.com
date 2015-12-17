804d4ab6f16934b8fa3a1e27dadd98a4902ce75a
kulakowka <kulakowka@gmail.com>
2015-12-18 02:54:03 +0300

    clean up

--- assets/js/handlers/comment.js
+++ assets/js/handlers/comment.js
@@ -1,15 +1,16 @@
+'use strict'
+
 var $ = require('jquery')
 
 module.exports.onFormSubmit = function onFormSubmit (event) {
   var form = $(this)
   var writeTab = form.find('.tab[data-frame="write"]')
-  var content = form.find('textarea[name="content"]')
   var data = form.serialize()
   var commentsList = $('.commentsList')
 
-  $.post('/comments', data).done(function(html) {
+  $.post('/comments', data).done(function (html) {
     commentsList.append(html)
-  }).fail(function(html) {
+  }).fail(function (html) {
     console.log('error')
   })
 
