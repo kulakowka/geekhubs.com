50a6c4759bd67fb6f1dc22fb45a3114b1696278f
kulakowka <kulakowka@gmail.com>
2015-12-18 17:57:49 +0300

    clean up

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,9 @@
+'use strict'
+
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/geekhub', function() {
-  
-  // return;  // drop database and populate data?
-  // mongoose.connection.db.dropDatabase(function(err, result) { 
-  //   console.log('DB dropped')
-  // })
+mongoose.connect('mongodb://localhost/geekhub', function () {
+  console.log('Mongo connected')
 })
 
 module.exports = mongoose
